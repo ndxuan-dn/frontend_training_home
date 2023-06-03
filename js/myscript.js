cce2a569d60fcc3f1f395f97c27c72f9d967784c
ndxuan-dn <ngoducxuan.danang@gmail.com>
2023-06-03 21:42:51 +0700

    Update code jquery for day4

--- js/myscript.js
+++ js/myscript.js
@@ -15,3 +15,44 @@
 // var input = $('input');
 // console.log(input.val('noi dung duoc thay doi'));
 var swiper = new Swiper(".mySwiper", {});
+
+
+$('#form').hide();
+$('#form').validate();
+$("#form").on('submit', (e) => {
+    e.preventDefault();
+    console.log(e);
+    let tensinhvien = $('#tensinhvien').val();
+    let lop =  $('#lop').val();
+    let namsinh =  $('#namsinh').val();
+    let newData = {
+        tensinhvien,
+        lop,
+        namsinh
+    }
+
+    axios.post('http://localhost:3333/sinhviens', newData)
+    .then(function (response) {
+        console.log(response);
+        // $('#tensinhvien, #lop, #namsinh').val('');
+        // window.location.href = '/';
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    // console.log(e)
+
+    // https://axios-http.com/vi/docs/example
+    // https://www.npmjs.com/package/json-server
+    // https://jqueryui.com/
+
+    // C -> POST
+    // R -> GET
+    // U -> PUT
+    // D -> DELETE
+
+})
+
+$('.showform').on('click', () => $('#form').toggle())
+
